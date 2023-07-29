a2ac30a6994b6ac763cbf5806c38a4d4a599e3e9
ijwgt <66661966@bk.ru>
2023-07-29 08:41:08 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D = Math.pow(b, 2) - 4 * a * c;
+  if (D < 0) {
+	arr.push(null);
+  } else if (D === 0) {
+	D = -b / (2 * a);
+	arr.push(D);
+  } else if (D > 0) {
+	let D1 = (-b + Math.sqrt(D) ) / (2 * a);
+	let D2 = (-b - Math.sqrt(D) ) / (2 * a);
+	arr.push(D1, D2);
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
+
