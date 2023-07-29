26d0b9ac7b035bf24fed32a25c73c9a7927476ca
ijwgt <66661966@bk.ru>
2023-07-29 20:57:24 +0300

    changed task #1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,14 +3,15 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let D = Math.pow(b, 2) - 4 * a * c;
+  
   if (D < 0) {
-	arr.push(null);
+	arr.push();
   } else if (D === 0) {
 	D = -b / (2 * a);
 	arr.push(D);
   } else if (D > 0) {
-	let D1 = (-b + Math.sqrt(D) ) / (2 * a);
-	let D2 = (-b - Math.sqrt(D) ) / (2 * a);
+	let D1 = (-b + Math.sqrt(D)) / (2 * a);
+	let D2 = (-b - Math.sqrt(D)) / (2 * a);
 	arr.push(D1, D2);
   }
 
