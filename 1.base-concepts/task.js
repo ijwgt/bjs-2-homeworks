c1fbb02076d52921466b940980e9742f49bb060e
ijwgt <66661966@bk.ru>
2023-07-31 22:21:39 +0300

    rework task#2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,21 +1,21 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  let D = Math.pow(b, 2) - 4 * a * c;
+	let arr = [];
+	let D = Math.pow(b, 2) - 4 * a * c;
 
-  if (D < 0) {
-	D = false;
-  } else if (D === 0) {
-	D = -b / (2 * a);
-	arr.push(D);
-  } else if (D > 0) {
-	let D1 = (-b + Math.sqrt(D)) / (2 * a);
-	let D2 = (-b - Math.sqrt(D)) / (2 * a);
-	arr.push(D1, D2);
-  }
+	if (D === 0) {
+		D = -b / (2 * a);
+		arr.push(D);
+	} else if (D > 0) {
+		let D1 = (-b + Math.sqrt(D)) / (2 * a);
+		let D2 = (-b - Math.sqrt(D)) / (2 * a);
+		arr.push(D1, D2);
+	} else {
+		D = false;
+	}
 
-  return arr;
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
