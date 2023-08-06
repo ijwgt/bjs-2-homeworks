62f2f5df766888b9adbab9f9a2c413ca75417979
ijwgt <66661966@bk.ru>
2023-08-06 19:57:41 +0300

    Initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,17 @@
 function getArrayParams(...arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	let avg = 0;
 
-  return { min: min, max: max, avg: avg };
+	for (i = 0; i < arr.length; i += 1) {
+		min = Math.min(min, arr[i]);
+		max = Math.max(max, arr[i]);
+		sum += arr[i];
+	}
+	avg = +(sum/arr.length).toFixed(2);
+
+	return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
