14f591c67d368cfd8556739f3c0ece7f46a255f1
ijwgt <66661966@bk.ru>
2023-08-13 12:16:33 +0300

    added task#2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -22,19 +22,59 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
+	let sum = arr.reduce(function (currentSum, currentNumber) {
+	return currentSum + currentNumber
+});
 
+	return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
+	let difference = max - min;
+	let min = Infinity;
+	let max = -Infinity;
 
+	if (arr.length === 0) return 0;
+
+	min = Math.min(min, ...arr);
+	max = Math.max(max, ...arr);
+
+	return difference;
 }
 
 function differenceEvenOddWorker(...arr) {
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
 
+	if (arr.length === 0) return 0;
+
+	for (i = 0; i < arr.length; i +=1) {
+		if (i % 2 === 0) {
+			sumEvenElement += arr[i];
+		}
+		else {
+			sumOddElement += arr[i];
+		}
+	}
+
+	return differenceMaxMinWorker();
 }
 
 function averageEvenElementsWorker(...arr) {
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+	let avg = sumEvenElement - countEvenElement;
+	
+	if (arr.length === 0) return 0;
 
+	for (i = 0; i < arr.length; i += 1) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+			countEvenElement += 1;
+		} 
+	}
+
+	return avg;
 }
 
 function makeWork (arrOfArr, func) {
